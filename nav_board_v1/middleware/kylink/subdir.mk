678ce17955b0ea4a875faba419d6b59c4bbfd396
kyChu <hello.kychu@gmail.com>
2019-04-23 16:28:48 +0800

    optimize kylink

--- nav_board_v1/middleware/kylink/subdir.mk
+++ nav_board_v1/middleware/kylink/subdir.mk
@@ -18,4 +18,4 @@ OBJ_DIRS = $(sort $(dir $(OBJS)))
 # Each subdirectory must supply rules for building sources it contributes
 $(BuildPath)/middleware/kylink/%.o: ./middleware/kylink/%.c | $(OBJ_DIRS)
 	@echo ' CC $<'
-	$(CC) $(PLATFORM) $(DEFS) $(INCS) $(CFGS) -O0 $(DBGS) -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	$(CC) $(PLATFORM) $(DEFS) $(INCS) $(CFGS) -Os $(DBGS) -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
