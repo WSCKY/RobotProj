98d59cff37c586a6c157d36cdf45bc871a767472
kyChu <hello.kychu@gmail.com>
2019-06-11 19:51:57 +0800

    add new package for debug

--- tiny_imu/sysconfig/LinkIf_conf.h
+++ tiny_imu/sysconfig/LinkIf_conf.h
@@ -15,10 +15,12 @@
 
 #define KYLINK_USER_TYPE               TYPE_IMU_INFO_Resp = 0x11, \
                                        TYPE_ATT_QUAT_Resp = 0x12, \
-									   TYPE_UPGRADE_REQUEST = 0x80
+									   TYPE_UPGRADE_REQUEST = 0x80, \
+									   TYPE_DEBUG_DATA_Resp = 0xDC,
 
 #define KYLINK_USER_TYPE_DATA          IMU_INFO_DEF IMU_InfoData; \
                                        Quat_T AttitudeQuat; \
-                                       FWInfoDef FileInfo;
+                                       FWInfoDef FileInfo; \
+                                       DebugFloatDef DebugData;
 
 #endif /* LINKIF_CONF_H_ */
