eb43bc845fd4feabcf2d74a3b7aff5addac7369a
kyChu <hello.kychu@gmail.com>
2019-12-18 15:40:03 +0800

    add i2c communication callback func

--- rtk_gnss_v1/rover/driver/bsp/src/callback.c
+++ rtk_gnss_v1/rover/driver/bsp/src/callback.c
@@ -82,6 +82,42 @@ void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *SpiHandle)
 }
 
 /**
+  * @brief  Tx Transfer completed callback.
+  * @param  I2cHandle: I2C handle.
+  * @retval None
+  */
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *I2cHandle)
+{
+  if(I2cHandle->Instance == MAG_I2C) {
+    magif_txcplt_callback(I2cHandle);
+  }
+}
+
+/**
+  * @brief  Rx Transfer completed callback.
+  * @param  I2cHandle: I2C handle
+  * @retval None
+  */
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *I2cHandle)
+{
+  if(I2cHandle->Instance == MAG_I2C) {
+    magif_rxcplt_callback(I2cHandle);
+  }
+}
+
+/**
+  * @brief  I2C error callbacks.
+  * @param  I2cHandle: I2C handle
+  * @retval None
+  */
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *I2cHandle)
+{
+  if(I2cHandle->Instance == MAG_I2C) {
+    magif_error_callback(I2cHandle);
+  }
+}
+
+/**
   * @brief  EXTI line detection callbacks.
   * @param  GPIO_Pin Specifies the pins connected EXTI line
   * @retval None
