10943dbf99cd2e7b18273b99a7fad70b528088b2
kyChu <hello.kychu@gmail.com>
2019-12-18 10:07:56 +0800

    update apps

--- rtk_gnss_v1/rover/apps/src/apps.c
+++ rtk_gnss_v1/rover/apps/src/apps.c
@@ -1,10 +1,7 @@
 #include "apps.h"
 #include "test_case.h"
 
-osThreadId LEDThread1Handle, LEDThread2Handle;
-
-static void LED_Thread1(void const *argument);
-static void LED_Thread2(void const *argument);
+static void led_thread(void const *argument);
 
 #define STR1(R) #R
 #define STR2(R) STR1(R)
@@ -55,88 +52,21 @@ void APP_StartThread(void const *argument)
   if(osThreadCreate(osThread(MESG), NULL) == NULL) ky_err("mesg task create failed.\n");
 
   /* LED INDICATOR TASK */
-  osThreadDef(LED_BLUE, LED_Thread1, osPriorityNormal, 0, configMINIMAL_STACK_SIZE); // stack size = 128B
-  osThreadDef(LED_GREEN, LED_Thread2, osPriorityNormal, 0, configMINIMAL_STACK_SIZE); // stack size = 128B
+  osThreadDef(LEDS, led_thread, osPriorityNormal, 0, configMINIMAL_STACK_SIZE); // stack size = 128B
 
-  LEDThread1Handle = osThreadCreate(osThread(LED_BLUE), NULL); if(LEDThread1Handle == NULL) ky_err("led1 task create failed.\n");
-  LEDThread2Handle = osThreadCreate(osThread(LED_GREEN), NULL); if(LEDThread2Handle == NULL) ky_err("led2 task create failed.\n");
+  if(osThreadCreate(osThread(LEDS), NULL) == NULL) ky_err("leds task create failed.\n");
 
   vTaskDelete(NULL);
 }
 
-/**
-  * @brief  Toggle LED_RED thread
-  * @param  Thread not used
-  * @retval None
-  */
-static void LED_Thread1(void const *argument)
+static void led_thread(void const *argument)
 {
-  uint32_t count = 0;
   (void) argument;
-  
-  for(;;)
-  {
-    count = osKernelSysTick() + 6000;
-    
-    /* Toggle LED_BLUE every 200 ms for 6 s */
-    while (count > osKernelSysTick())
-    {
-      led_toggle(LED_BLUE);
-      
-      osDelay(200);
-    }
-    
-    /* Turn off LED_BLUE */
-    led_off(LED_BLUE);
-    
-    /* Suspend Thread 1 */
-    osThreadSuspend(NULL);
-    
-    count = osKernelSysTick() + 6000;
-    
-    /* Toggle LED_RED every 400 ms for 6 s */
-    while (count > osKernelSysTick())
-    {
-      led_toggle(LED_BLUE);
 
-      osDelay(400);
-    }
-    
-    /* Resume Thread 2 */
-    osThreadResume(LEDThread2Handle); 
-  }
-}
+  for(;;) {
+    led_toggle(LED_GREEN);
 
-/**
-  * @brief  Toggle LED_GREEN thread
-  * @param  argument not used
-  * @retval None
-  */
-static void LED_Thread2(void const *argument)
-{
-  uint32_t count;
-  (void) argument;
-  
-  for(;;)
-  {
-    count = osKernelSysTick() + 12000;
-    
-    /* Toggle LED_GREEN every 500 ms for 12 s */
-    while (count > osKernelSysTick())
-    {
-      led_toggle(LED_GREEN);
-      
-      osDelay(500);
-    }
-    
-    /* Turn off LED_GREEN */
-    led_off(LED_GREEN);
-    
-    /* Resume Thread 1 */
-    osThreadResume(LEDThread1Handle);
-
-    /* Suspend Thread 2 */
-    osThreadSuspend(NULL);  
+    osDelay(400);
   }
 }
 
