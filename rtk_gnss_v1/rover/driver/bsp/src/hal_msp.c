a5698a80205b35639f9fb328be95822e80f03261
kyChu <hello.kychu@gmail.com>
2019-12-18 15:37:56 +0800

    add i2c msp driver

--- rtk_gnss_v1/rover/driver/bsp/src/hal_msp.c
+++ rtk_gnss_v1/rover/driver/bsp/src/hal_msp.c
@@ -96,4 +96,36 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef *hspi)
   }
 }
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/**
+  * @brief I2C MSP Initialization
+  *        This function configures the hardware resources used in this example:
+  *           - Peripheral's clock enable
+  *           - Peripheral's GPIO Configuration
+  *           - DMA configuration for transmission request by peripheral
+  *           - NVIC configuration for DMA interrupt request enable
+  * @param hi2c: I2C handle pointer
+  * @retval None
+  */
+void HAL_I2C_MspInit(I2C_HandleTypeDef *hi2c)
+{
+  if(hi2c->Instance == MAG_I2C) {
+    magif_msp_init(hi2c);
+  }
+}
+
+/**
+  * @brief I2C MSP De-Initialization
+  *        This function frees the hardware resources used in this example:
+  *          - Disable the Peripheral's clock
+  *          - Revert GPIO, DMA and NVIC configuration to their default state
+  * @param hi2c: I2C handle pointer
+  * @retval None
+  */
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef *hi2c)
+{
+  if(hi2c->Instance == MAG_I2C) {
+    magif_msp_deinit(hi2c);
+  }
+}
+
+/******************** kyChu<kyChu@qq.com> **** END OF FILE ********************/
