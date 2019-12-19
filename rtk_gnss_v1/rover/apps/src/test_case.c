96666cf2a7590957bfb14012756e5e998a345bdd
kyChu <hello.kychu@gmail.com>
2019-12-19 14:54:49 +0800

    add ist83xx test code

--- rtk_gnss_v1/rover/apps/src/test_case.c
+++ rtk_gnss_v1/rover/apps/src/test_case.c
@@ -5,6 +5,7 @@
 #include "cpu_utils.h"
 
 void scan_i2c_dev(void);
+void check_ist83xx(void);
 
 void test_case_task(void const *argument)
 {
@@ -12,9 +13,50 @@ void test_case_task(void const *argument)
   ky_info("test case task start.\n");
   osDelay(500);
 
-  scan_i2c_dev();
+//  scan_i2c_dev();
+  check_ist83xx();
 
   vTaskDelete(NULL);
+}
+
+void check_ist83xx(void)
+{
+  int cnt = 0;
+  _3AxisRaw *ist_raw_a = kmm_alloc(sizeof(_3AxisRaw));
+  _3AxisRaw *ist_raw_b = kmm_alloc(sizeof(_3AxisRaw));
+  ist83xx_dev_t *ist8310_a = kmm_alloc(sizeof(ist83xx_dev_t));
+  ist83xx_dev_t *ist8310_b = kmm_alloc(sizeof(ist83xx_dev_t));
+  if((ist8310_a == NULL) || (ist8310_b == NULL)) {
+    ky_err("ist8310 memory alloc failed. EXIT!\n");
+    return;
+  }
+
+  ist8310_a->dev_addr = 0x1A;
+  ist8310_b->dev_addr = 0x1C;
+  ist8310_a->io_init = ist8310_b->io_init = magif_init;
+  ist8310_a->io_ready = ist8310_b->io_ready = magif_check_ready;
+  ist8310_a->read_reg = ist8310_b->read_reg = magif_read_mem_dma;
+  ist8310_a->write_reg = ist8310_b->write_reg = magif_write_mem_dma;
+
+  ky_info("init ist8310 a.\n");
+  if(ist83xx_init(ist8310_a) != status_ok) {
+    ky_err("ist8310 a init failed.\n");
+  }
+  ky_info("init ist8310 b.\n");
+  if(ist83xx_init(ist8310_b) != status_ok) {
+    ky_err("ist8310 b init failed.\n");
+  }
+
+  for(;;) {
+    osDelay(5);
+    ist83xx_read_data(ist8310_a, ist_raw_a);
+    ist83xx_read_data(ist8310_b, ist_raw_b);
+    cnt ++;
+    if(cnt >= 200) {
+      cnt = 0;
+      ky_info("%3d, %3d, %3d;  %3d, %3d, %3d\n", ist_raw_a->X, ist_raw_a->Y, ist_raw_a->Z, ist_raw_b->X, ist_raw_b->Y, ist_raw_b->Z);
+    }
+  }
 }
 
 void scan_i2c_dev(void)
@@ -22,7 +64,7 @@ void scan_i2c_dev(void)
   uint8_t devaddr = 0x00;
   uint8_t reg_val = 0x00;
   if(magif_init() != status_ok) {
-    ky_info("mag if init failed. EXIT!\n");
+    ky_err("mag if init failed. EXIT!\n");
     return;
   }
 
