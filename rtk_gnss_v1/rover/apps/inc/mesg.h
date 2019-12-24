66eee3bc23c2a801fe2a00bb26354dc3f42444c1
kyChu <hello.kychu@gmail.com>
2019-12-24 16:44:13 +0800

    add a common message type

--- rtk_gnss_v1/rover/apps/inc/mesg.h
+++ rtk_gnss_v1/rover/apps/inc/mesg.h
@@ -3,6 +3,19 @@
 
 #include "drivers.h"
 
+typedef enum {
+  msg_read = 0,
+  msg_write = 1
+} msg_wr_state;
+
+__PACK_BEGIN typedef struct {
+  uint32_t type;
+  uint32_t len;
+  void *state;
+  void *pointer;
+} __PACK_END COM_MSG_DEF;
+
 void mesg_send_task(void const *argument);
+void mesg_send_mesg(void *msg);
 
 #endif /* __MESG_H */
