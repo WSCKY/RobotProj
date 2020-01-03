5dfcfa798333d8a6e9040554bb034d0022d18b21
kyChu <hello.kychu@gmail.com>
2020-01-03 10:56:46 +0800

    config rtos heap size as 64KB

--- rtk_gnss_v1/rover/sysconfig/boardconfig.h
+++ rtk_gnss_v1/rover/sysconfig/boardconfig.h
@@ -22,10 +22,7 @@
 #define SYS_SRAM_ORIGIN                     0x20000000
 #define SYS_SRAM_LENGTH                     256K
 
-/*  Total size of IN buffer:  Total size of USB IN buffer: APP_RX_DATA_SIZE*8/MAX_BAUDARATE*1000 should be > CDC_IN_FRAME_INTERVAL */
-//#define APP_RX_DATA_SIZE                    768
-
-//#define UART2_RX_CACHE_SIZE                 88
+#define RTOS_HEAP_DEPTH                     ( 64 * 1024 )
 
 #define SYS_MAIN_FLASH                      FLASH_TEXT
 #define SYS_MAIN_MEMORY                     SRAM
