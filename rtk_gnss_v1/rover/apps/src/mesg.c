e1a44184fbdbe3ccd4d7f0d8bf3056e078c9348a
kyChu <hello.kychu@gmail.com>
2020-01-03 15:31:43 +0800

    example for kylink decode

--- rtk_gnss_v1/rover/apps/src/mesg.c
+++ rtk_gnss_v1/rover/apps/src/mesg.c
@@ -3,13 +3,14 @@
 #include "gnss.h"
 #include <math.h>
 
-static KYLINK_CORE_HANDLE kylink_msg;
-static kyLinkPackageDef tx_packet;
+static KYLINK_CORE_HANDLE *kylink_msg;
+static kyLinkPackageDef *tx_packet;
 
 /* Semaphore to signal incoming packets */
 static QueueHandle_t com_msg_q = NULL;
 
 static void mesg_decode_task(void const *argument);
+static void mesg_decode_callback(kyLinkPackageDef *pRx);
 
 void mesg_send_task(void const *argument)
 {
@@ -28,28 +29,31 @@ void mesg_send_task(void const *argument)
   }
 
   msg = kmm_alloc(sizeof(COM_MSG_DEF));
-  if(msg == NULL) {
-    ky_err("alloc msg memory failed, EXIT!\n");
+  tx_packet = kmm_alloc(sizeof(kyLinkPackageDef));
+  kylink_msg = kmm_alloc(sizeof(KYLINK_CORE_HANDLE));
+
+  if(msg == NULL || tx_packet == NULL || kylink_msg == NULL) {
+    ky_err("mesg memory alloc failed, EXIT!\n");
     vTaskDelete(NULL);
   }
 
   ky_info("mesg module started.\n");
 
-  kyLinkInit(&kylink_msg);
-  kyLinkConfigTxFunc(&kylink_msg, cdcif_tx_bytes);
-  kyLinkInitPackage(&tx_packet);
+  kyLinkInit(kylink_msg);
+  kyLinkConfigTxFunc(kylink_msg, cdcif_tx_bytes);
+  kyLinkInitPackage(tx_packet);
 
   osThreadDef(DECODE, mesg_decode_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2); // stack size = 256B
   if(osThreadCreate(osThread(DECODE), NULL) == NULL) ky_err("mesg decode task create failed.\n");
 
   for(;;) {
     if(xQueueReceive(com_msg_q, msg, osWaitForever) == pdPASS) {
-      tx_packet.FormatData.length = msg->len;
-      tx_packet.FormatData.msg_id = msg->type;
-      ( void ) memcpy( ( void * ) &tx_packet.FormatData.PacketData.RawData[0], ( void * ) msg->pointer, ( size_t ) msg->len );
+      tx_packet->FormatData.length = msg->len;
+      tx_packet->FormatData.msg_id = msg->type;
+      ( void ) memcpy( ( void * ) &(tx_packet->FormatData.PacketData.RawData[0]), ( void * ) msg->pointer, ( size_t ) msg->len );
       // indicate read operation.
       *((msg_wr_state *)(msg->state)) = msg_read;
-      SendTxPacket(&kylink_msg, &tx_packet);
+      SendTxPacket(kylink_msg, tx_packet);
     }
   }
 }
@@ -60,23 +64,27 @@ void mesg_send_mesg(void *msg)
   xQueueSend(com_msg_q, msg, osWaitForever);
 }
 
+static void mesg_decode_callback(kyLinkPackageDef *pRx)
+{
+  ky_info("recv new mesg: 0x%x, 0x%x.\n", pRx->FormatData.msg_id, pRx->FormatData.dev_id);
+}
+
 static void mesg_decode_task(void const *argument)
 {
-  int rx_len;
+  int decode_cnt, rx_len;
   uint8_t *pRxCache;
-  pRxCache = kmm_alloc(32);
+  pRxCache = kmm_alloc(128);
   if(pRxCache == NULL) {
     ky_err("no memory for mesg decoder task.\n");
     vTaskDelete(NULL);
   }
+  kyLinkConfigCbFunc(kylink_msg, mesg_decode_callback);
   ky_info("mesg decoder start.\n");
   for(;;) {
-    rx_len = cdcif_rx_bytes(pRxCache, 31, osWaitForever);
-    if(rx_len != 0) {
-      pRxCache[rx_len] = 0;
-      ky_info("recv %dB: %s\n", rx_len, pRxCache);
-    } else {
-      ky_err("mesg recv error.\n");
+    rx_len = cdcif_rx_bytes(pRxCache, 128, osWaitForever);
+    decode_cnt = 0;
+    while(decode_cnt < rx_len) {
+      kylink_decode(kylink_msg, pRxCache[decode_cnt ++]);
     }
   }
 }
