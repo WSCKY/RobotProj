2b2472280fe1a89497d92710e42de555ad7cc89f
kyChu <hello.kychu@gmail.com>
2020-01-07 19:49:59 +0800

    update mesg proc module

--- rtk_gnss_v1/rover/apps/inc/mesg.h
+++ rtk_gnss_v1/rover/apps/inc/mesg.h
@@ -3,18 +3,6 @@
 
 #include "drivers.h"
 
-typedef enum {
-  msg_read = 0,
-  msg_write = 1
-} msg_wr_state;
-
-__PACK_BEGIN typedef struct {
-  uint32_t type;
-  uint32_t len;
-  void *state;
-  void *pointer;
-} __PACK_END COM_MSG_DEF;
-
 #define TYPE_QUAT_Info_Resp                      (0x12)
 #define TYPE_RTK_Info_Resp                       (0x61)
 #define TYPE_PVTS_Info_Resp                      (0x56)
@@ -23,7 +11,7 @@ __PACK_BEGIN typedef struct {
 #define FILE_TRANS_DATA_MSG                      (0xB1)
 #define FILE_TRANS_ACK_MSG                       (0xB2)
 
-void mesg_send_task(void const *argument);
-void mesg_send_mesg(void *msg);
+void mesg_proc_task(void const *argument);
+void mesg_send_mesg(const void *msg, uint8_t msgid, uint16_t len);
 
 #endif /* __MESG_H */
--- rtk_gnss_v1/rover/apps/src/mesg.c
+++ rtk_gnss_v1/rover/apps/src/mesg.c
@@ -6,98 +6,105 @@
 
 static KYLINK_CORE_HANDLE *kylink_msg;
 
-static uint8_t *mesg_data_cache;
 static uint8_t *kylink_decoder_cache;
-
 static uint8_t *mesg_test_send_cache;
 
-#define KYLINK_MSG_CACHE_SIZE                    (128)
+#define KYLINK_DECODER_CACHE_SIZE                (128)
+#define KYLINK_MSG_CACHE_SIZE                    (768)
 
+static osMutexId msg_mutex = NULL;
 /* Semaphore to signal incoming packets */
-static QueueHandle_t com_msg_q = NULL;
+static osSemaphoreId msg_available = NULL;
+
+static uint32_t _task_running = 0;
 
 static void mesg_decode_task(void const *argument);
 static void mesg_decode_callback(kyLinkBlockDef *pRx);
 
 static uint32_t msg_data_length = 0;
-static status_t test_mesg_cache_send(uint8_t *p, uint32_t l);
+static status_t mesg_send_cache(uint8_t *p, uint32_t l);
 
-void mesg_send_task(void const *argument)
+void mesg_proc_task(void const *argument)
 {
-  COM_MSG_DEF *msg = NULL;
   kyLinkConfig_t *cfg = NULL;
 
   if(cdcif_init() != status_ok) {
-    ky_err("usb cdc init failed.\n");
-    ky_err("mesg module start failed, EXIT!\n");
+    ky_err("usb cdc init failed, EXIT!\n");
     vTaskDelete(NULL);
   }
 
-  com_msg_q = xQueueCreate(10, sizeof(COM_MSG_DEF));
-  if(com_msg_q == NULL) {
-    ky_err("creates queue instance failed, EXIT!\n");
+  osMutexDef(MSG_MUTEX);
+  msg_mutex = osMutexCreate(osMutex(MSG_MUTEX));
+  if(msg_mutex == NULL) {
+    ky_err("create MSG MUTEX failed, EXIT!\n");
     vTaskDelete(NULL);
   }
 
-  msg = kmm_alloc(sizeof(COM_MSG_DEF));
+  osSemaphoreDef(MSG_SEMA);
+  msg_available = osSemaphoreCreate(osSemaphore(MSG_SEMA), 1);
+  if(msg_available == NULL) {
+    ky_err("create MSG SEMA failed, EXIT!\n");
+    osMutexDelete(msg_mutex);
+    vTaskDelete(NULL);
+  }
+
   cfg = kmm_alloc(sizeof(kyLinkConfig_t));
   kylink_msg = kmm_alloc(sizeof(KYLINK_CORE_HANDLE));
-  mesg_data_cache = kmm_alloc(KYLINK_MSG_CACHE_SIZE);
-  kylink_decoder_cache = kmm_alloc(KYLINK_MSG_CACHE_SIZE);
-
-  mesg_test_send_cache = kmm_alloc(KYLINK_MSG_CACHE_SIZE + 8);
-
-  if(msg == NULL || cfg == NULL || kylink_msg == NULL || mesg_data_cache == NULL || kylink_decoder_cache == NULL || mesg_test_send_cache == NULL) {
-    kmm_free(msg);
+  mesg_test_send_cache = kmm_alloc(KYLINK_MSG_CACHE_SIZE);
+  kylink_decoder_cache = kmm_alloc(KYLINK_DECODER_CACHE_SIZE);
+  if(cfg == NULL || kylink_msg == NULL || kylink_decoder_cache == NULL || mesg_test_send_cache == NULL) {
     kmm_free(cfg);
     kmm_free(kylink_msg);
-    kmm_free(mesg_data_cache);
     kmm_free(kylink_decoder_cache);
     kmm_free(mesg_test_send_cache);
-    ky_err("mesg memory alloc failed, EXIT!\n");
+    osMutexDelete(msg_mutex);
+    osSemaphoreDelete(msg_available);
+    ky_err("MSG memory alloc failed, EXIT!\n");
     vTaskDelete(NULL);
   }
 
-  cfg->txfunc = test_mesg_cache_send;//cdcif_tx_bytes;
+  cfg->txfunc = mesg_send_cache;
   cfg->callback = mesg_decode_callback;
-  cfg->cache_size = KYLINK_MSG_CACHE_SIZE;
   cfg->decoder_cache = kylink_decoder_cache;
+  cfg->cache_size = KYLINK_DECODER_CACHE_SIZE;
   kylink_init(kylink_msg, cfg);
 
   kmm_free(cfg);
 
-  ky_info("mesg module started.\n");
+  _task_running = 1;
+  ky_info("mesg module start.\n");
 
-  osThreadDef(DECODE, mesg_decode_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2); // stack size = 256B
+  osThreadDef(DECODE, mesg_decode_task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE); // stack size = 128B
   if(osThreadCreate(osThread(DECODE), NULL) == NULL) ky_err("mesg decode task create failed.\n");
 
   for(;;) {
-    if(xQueueReceive(com_msg_q, msg, osWaitForever) == pdPASS) {
-      ( void ) memcpy( ( void * ) mesg_data_cache, ( void * ) msg->pointer, ( size_t ) msg->len );
-      // indicate read operation.
-      *((msg_wr_state *)(msg->state)) = msg_read;
-      kylink_send(kylink_msg, ( void * ) mesg_data_cache, msg->type, msg->len);
+    if(osSemaphoreWait(msg_available, osWaitForever) == osOK) {
+      osMutexWait(msg_mutex, osWaitForever);
       if(msg_data_length > 0) {
         cdcif_tx_bytes(mesg_test_send_cache, msg_data_length);
         msg_data_length = 0;
       }
+      osMutexRelease(msg_mutex);
     }
   }
 }
 
-static status_t test_mesg_cache_send(uint8_t *p, uint32_t l)
+static status_t mesg_send_cache(uint8_t *p, uint32_t l)
 {
-  if((msg_data_length + l) < (KYLINK_MSG_CACHE_SIZE + 8)) {
+  if((msg_data_length + l) < (KYLINK_MSG_CACHE_SIZE)) {
     memcpy((void *)mesg_test_send_cache + msg_data_length, (void *)p, (size_t)l);
     msg_data_length += l;
   }
   return status_ok;
 }
 
-void mesg_send_mesg(void *msg)
+void mesg_send_mesg(const void *msg, uint8_t msgid, uint16_t len)
 {
-  if(com_msg_q == NULL) return;
-  xQueueSend(com_msg_q, msg, osWaitForever);
+  if(_task_running == 0) return;
+  osMutexWait(msg_mutex, osWaitForever);
+  kylink_send(kylink_msg, ( void * ) msg, msgid, len);
+  osMutexRelease(msg_mutex);
+  osSemaphoreRelease(msg_available);
 }
 
 static void mesg_decode_callback(kyLinkBlockDef *pRx)
