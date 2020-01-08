6eff0d7966f72511060c10397afdee9633e22fcb
luzili <luzili@dalideMBP.lan>
2020-01-08 23:02:03 +0800

    add magnetics process module

---
+++ rtk_gnss_v1/rover/apps/inc/compass.h
@@ -0,0 +1,16 @@
+/*
+ * compass.h
+ *
+ *  Created on: Jan 8, 2020
+ *      Author: kychu
+ */
+
+#ifndef __COMPASS_H
+#define __COMPASS_H
+
+void magnetics_task(void const *argument);
+
+#endif /* __COMPASS_H */
+
+/******************** kyChu<kyChu@qq.com> **** END OF FILE ********************/
+
---
+++ rtk_gnss_v1/rover/apps/src/compass.c
@@ -0,0 +1,18 @@
+/*
+ * compass.c
+ *
+ *  Created on: Jan 8, 2020
+ *      Author: kychu
+ */
+
+#include "compass.h"
+
+void magnetics_task(void const *argument)
+{
+  for(;;) {
+    osDelay(1000);
+  }
+}
+
+/******************** kyChu<kyChu@qq.com> **** END OF FILE ********************/
+
